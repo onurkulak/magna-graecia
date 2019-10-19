72de401aeed3d4b70367704cba0b38d085519ea1
onur <kulaksizoglu.onur@gmail.com>
2019-10-19 04:32:05 -0400

    manage trade operations

--- src/GameEngine/Shipment.java
+++ src/GameEngine/Shipment.java
@@ -10,7 +10,68 @@ package GameEngine;
  * @author onur
  */
 public class Shipment {
-    private int monthsLeft;
+
+    private int monthsLeft, transferDuration;
     private TradeOffer deal;
     private Faction accepter;
+
+    public Shipment(int transferDuration, TradeOffer deal, Faction accepter) {
+        this.transferDuration = transferDuration;
+        this.deal = deal;
+        this.accepter = accepter;
+        monthsLeft = transferDuration;
+        // shipment is added to the lists of both parties
+        // but offer needs to be removed by the manager
+        accepter.getShipments().add(this);
+        deal.getOfferer().getShipments().add(this);
+        // resource and gold are transferred
+        // if offerer is selling
+        if (!getDeal().isBuyer()) {
+            getDeal().getOfferer().changeResource(
+                    getDeal().getResourceTraded(), -getDeal().getAmount());
+            accepter.changeGold(-getDeal().getCost());
+        } else {
+            accepter.changeResource(
+                    getDeal().getResourceTraded(), -getDeal().getAmount());
+            getDeal().getOfferer().changeGold(-getDeal().getCost());
+        }
+    }
+
+    public int getMonthsLeft() {
+        return monthsLeft;
+    }
+
+    public int getTransferDuration() {
+        return transferDuration;
+    }
+
+    public TradeOffer getDeal() {
+        return deal;
+    }
+
+    public Faction getAccepter() {
+        return accepter;
+    }
+
+    public boolean moveShipment() {
+        monthsLeft--;
+        if (monthsLeft <= 0) {
+            // resource and gold are transferred
+            if (getDeal().isBuyer()) {
+                getDeal().getOfferer().changeResource(
+                        getDeal().getResourceTraded(), getDeal().getAmount());
+                accepter.changeGold(getDeal().getCost());
+            } else {
+                accepter.changeResource(
+                        getDeal().getResourceTraded(), getDeal().getAmount());
+                getDeal().getOfferer().changeGold(getDeal().getCost());
+            }
+
+            // shipment is removed from the lists
+            accepter.getShipments().remove(this);
+            getDeal().getOfferer().getShipments().remove(this);
+            return true;
+        }
+        return false;
+    }
 }
--- src/GameEngine/TradeOffer.java
+++ src/GameEngine/TradeOffer.java
@@ -10,8 +10,36 @@ package GameEngine;
  * @author onur
  */
 public class TradeOffer {
-    private Resource resourceTraded;
-    private int cost, amount, travelDuraiton;
-    private boolean buyer;
-    private Faction offerer;
+    private final Resource resourceTraded;
+    private final int cost, amount;
+    private final boolean buyer;
+    private final Faction offerer;
+
+    public TradeOffer(Resource resourceTraded, int cost, int amount, boolean buyer, Faction offerer) {
+        this.resourceTraded = resourceTraded;
+        this.cost = cost;
+        this.amount = amount;
+        this.buyer = buyer;
+        this.offerer = offerer;
+    }
+
+    public Resource getResourceTraded() {
+        return resourceTraded;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public boolean isBuyer() {
+        return buyer;
+    }
+
+    public Faction getOfferer() {
+        return offerer;
+    }
 }
